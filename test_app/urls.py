98d960eb7c4b87ae809a4edb8f76cdf1b8b73d44
Rachel Gaiber <rachelgaiber55@gmail.com>
2017-02-26 17:28:00 -0800

    Initially login to Instagram instead of static HTML home page

--- test_app/urls.py
+++ test_app/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'test_app'
 urlpatterns = [
-	url(r'^$', views.home, name='home'),
+    url(r'^$', views.home, name='home'),
+    url(r'^authorized/$', views.authorized, name='authorized'),
 ]
--- test_app/views.py
+++ test_app/views.py
@@ -1,7 +1,40 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+
+from instagram.client import InstagramAPI
+
+
+import os, requests
+
+#https://api.instagram.com/oauth/authorize/?client_id=CLIENT-ID&redirect_uri=REDIRECT-URI&response_type=code
+client_id = '6e89b63d0f1444faadfdd9f4d198a989'
+client_secret = '279299f3b9d745fca8b7261a64289c8f'
 
 # Create your views here.
-
-
 def home(request):
-	return render(request, 'test_app/home.html')
+    redirect_uri = os.path.join(request.build_absolute_uri(), 'authorized/')
+    url = 'https://api.instagram.com/oauth/authorize/?client_id='
+    url += client_id + '&redirect_uri=' + redirect_uri + '&response_type=code'
+    return HttpResponseRedirect(url)
+
+def authorized(request):
+    code = request.GET.get('code')
+
+    #TODO: do this in better way, seems hacky
+    path = request.path
+    host = request.META['HTTP_HOST']
+    redirect_uri = redirect_uri = 'http://' + str(host) + str(path)
+
+    post_fields = {
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'grant_type': 'authorization_code',
+        'redirect_uri': redirect_uri,
+        'code': code
+    }
+    url = 'https://api.instagram.com/oauth/access_token'
+    r = requests.post(url, data=post_fields)
+    json = r.json()
+    access_token = json['access_token']
+    return HttpResponse('Access token:' + access_token)
