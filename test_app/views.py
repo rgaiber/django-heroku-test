3d14e52c69c571989c6c1cf93db9b821d16b9809
Rachel Gaiber <rachelgaiber55@gmail.com>
2017-02-27 15:55:37 -0800

    Use session instead of global variables in the view

--- test_app/views.py
+++ test_app/views.py
@@ -5,40 +5,39 @@ import os, requests
 
 client_id = '6e89b63d0f1444faadfdd9f4d198a989'
 client_secret = '279299f3b9d745fca8b7261a64289c8f'
-redirect_uri = None
 access_token = None
 
+REDIRECT_URI = 'redirect_uri'
+ACCESS_TOKEN = 'access_token'
+
 """Sets the redirect URL after being authorized if necessary"""
-def set_redirect_url(request):
-    global redirect_uri
+def set_redirect_uri(request):
     path = request.path
     host = request.META['HTTP_HOST']
-    redirect_uri = request.scheme + '://' + str(host) + str(path)
+    request.session[REDIRECT_URI] = request.scheme + '://' + str(host) + str(path)
 
 """Sets access_token which is used when requesting data from the API"""
 def set_access_token(request):
     code = request.GET.get('code')
-    global redirect_uri
-    global access_token
 
-    if redirect_uri == None:
-        set_redirect_url(request)
+    if REDIRECT_URI not in request.session:
+        set_redirect_uri(request)
 
     post_fields = {
         'client_id': client_id,
         'client_secret': client_secret,
         'grant_type': 'authorization_code',
-        'redirect_uri': redirect_uri,
+        'redirect_uri': request.session[REDIRECT_URI],
         'code': code
     }
     url = 'https://api.instagram.com/oauth/access_token'
     r = requests.post(url, data=post_fields)
     json = r.json()
-    access_token = json['access_token']
+    request.session[ACCESS_TOKEN] = json['access_token']
 
-def get_current_user_photos():
+def get_current_user_photos(request):
     url = 'https://api.instagram.com/v1/users/self/media/recent/?access_token='
-    url += access_token
+    url += request.session[ACCESS_TOKEN]
     r = requests.get(url)
     json = r.json()
     data = json['data']
@@ -49,16 +48,15 @@ def get_current_user_photos():
 
 # Create your views here.
 def home(request):
-    global redirect_uri
-    redirect_uri = os.path.join(request.build_absolute_uri(), 'authorized/')
+    uri = os.path.join(request.build_absolute_uri(), 'authorized/')
+    request.session[REDIRECT_URI] = uri
     url = 'https://api.instagram.com/oauth/authorize/?client_id='
-    url += client_id + '&redirect_uri=' + redirect_uri + '&response_type=code'
+    url += client_id + '&redirect_uri=' + uri + '&response_type=code'
     return HttpResponseRedirect(url)
 
 def authorized(request):
-    global access_token
-    if access_token == None:
+    if ACCESS_TOKEN not in request.session:
         set_access_token(request)
 
-    context = get_current_user_photos()
+    context = get_current_user_photos(request)
     return render(request, 'test_app/authorized.html', context)
